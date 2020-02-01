8608b7438b3760f5345817b5c204f7392513a7b9
viswajeetk <viswajeetk@INviswajeetkumars-MacBook-Pro.local>
2020-02-02 01:22:42 +0530

    Updates

---
+++ precache-manifest.61e67304cd233e0a1b1e9049a0e94187.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c17d1e30cc11a76dc2af8375bf856a51",
+    "url": "/gh-pages-react/index.html"
+  },
+  {
+    "revision": "9b24532211e8c7d694e3",
+    "url": "/gh-pages-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e081ec9363c5b60bd7c7",
+    "url": "/gh-pages-react/static/js/2.7fe597c1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/gh-pages-react/static/js/2.7fe597c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b24532211e8c7d694e3",
+    "url": "/gh-pages-react/static/js/main.4b7fa030.chunk.js"
+  },
+  {
+    "revision": "0b60e3826458b52c2f48",
+    "url": "/gh-pages-react/static/js/runtime-main.e30ec650.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-pages-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
